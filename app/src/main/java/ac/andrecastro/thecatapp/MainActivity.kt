bf3fc1191fafcaeddbc67bf89568b0c2b48586ad
andrecastrocode <33205006+andrecastrocode@users.noreply.github.com>
2022-06-16 22:41:42 +0100

    Test random request

--- app/src/main/java/ac/andrecastro/thecatapp/MainActivity.kt
+++ app/src/main/java/ac/andrecastro/thecatapp/MainActivity.kt
@@ -2,10 +2,30 @@ package ac.andrecastro.thecatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.coroutines.awaitStringResponse
+import com.google.gson.Gson
+import kotlinx.coroutines.runBlocking
+
+data class CatFact(var fact: String = "", var length: Int = 0) {
+
+    class Deserializer : ResponseDeserializable<CatFact> {
+        override fun deserialize(content: String): CatFact =
+            Gson().fromJson(content, CatFact::class.java)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        runBlocking {
+            Fuel.get("https://catfact.ninja/fact").appendHeader("accept: application/json")
+                .responseObject(CatFact.Deserializer()) { _, _, result ->
+                    println("FACT: " + result.component1()?.fact)
+                    println("LENGTH: " + result.component1()?.length)
+                }
+        }
         setContentView(R.layout.activity_main)
     }
 }
