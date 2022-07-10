55c9f1d0111bb6e7e789a71bc38a17ac4c977f06
andrecastrocode <33205006+andrecastrocode@users.noreply.github.com>
2022-07-10 19:07:30 +0100

    Added loading progress bar to cat image

--- app/src/main/java/ac/andrecastro/thecatapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/ac/andrecastro/thecatapp/presentation/activities/MainActivity.kt
@@ -3,19 +3,27 @@ package ac.andrecastro.thecatapp.presentation.activities
 import ac.andrecastro.thecatapp.R
 import ac.andrecastro.thecatapp.presentation.UiState
 import ac.andrecastro.thecatapp.presentation.viewmodel.CatFactViewModel
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var catFactTextView: TextView
     private lateinit var catImageImageView: ImageView
+    private lateinit var catFactTextView: TextView
+    private lateinit var catFactProgressBar: ProgressBar
     private lateinit var moreFactsButton: Button
     private lateinit var defaultCatFact: String
 
@@ -35,6 +43,7 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         catImageImageView = findViewById(R.id.cat_image)
         catFactTextView = findViewById(R.id.cat_fact)
+        catFactProgressBar = findViewById(R.id.cat_fact_progress_bar)
         moreFactsButton = findViewById(R.id.more_facts_button)
         defaultCatFact = getString(R.string.defaultCatFact)
     }
@@ -59,8 +68,31 @@ class MainActivity : AppCompatActivity() {
 
     // TODO: refactor this call
     private fun newImage() {
+        catFactProgressBar.visibility = View.VISIBLE
         Glide.with(this)
             .load("https://cataas.com/cat/gif")
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    catFactProgressBar.visibility = View.GONE
+                    return false
+                }
+
+            })
             .diskCacheStrategy(DiskCacheStrategy.NONE)
             .skipMemoryCache(true)
             .into(catImageImageView)
